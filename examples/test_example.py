9c0e2735efe510c77cec60ee2b3f1edde0638c02
Askhat Nuriddinov <askmyhat@yandex.ru>
2019-02-08 14:43:33 +0100

    Added license headers

--- examples/test_example.py
+++ examples/test_example.py
@@ -1,3 +1,35 @@
+#  Copyright (c) 2015 SONATA-NFV, 5GTANGO
+# ALL RIGHTS RESERVED.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Neither the name of the SONATA-NFV, 5GTANGO
+# nor the names of its contributors may be used to endorse or promote
+# products derived from this software without specific prior written
+# permission.
+#
+# This work has been performed in the framework of the SONATA project,
+# funded by the European Commission under Grant number 671517 through
+# the Horizon 2020 and 5G-PPP programmes. The authors would like to
+# acknowledge the contributions of their colleagues of the SONATA
+# partner consortium (www.sonata-nfv.eu).
+#
+# This work has also been performed in the framework of the 5GTANGO project,
+# funded by the European Commission under Grant number 761493 through
+# the Horizon 2020 and 5G-PPP programmes. The authors would like to
+# acknowledge the contributions of their colleagues of the SONATA
+# partner consortium (www.5gtango.eu).
+
 import pytest
 import datetime
 import time
@@ -9,10 +41,10 @@ from tangotest.utils import SNORT_TIME_FORMAT, SNIFFER_TIME_FORMAT
 @pytest.fixture(scope='module')
 def testbed():
     with Emulator() as vim:
-        vim.add_instance_from_image('client', 'tangoclient')
-        vim.add_instance_from_image('server', 'tangoserver')
-        vim.add_instance_from_image('firewall', 'tangofirewall', interfaces=['input', 'output'])
-        vim.add_instance_from_image('snort', 'tangosnort', interfaces=['input', 'output'])
+        vim.add_instance_from_source('client', 'tangoclient')
+        vim.add_instance_from_source('server', 'tangoserver')
+        vim.add_instance_from_source('firewall', 'tangofirewall', interfaces=['input', 'output'])
+        vim.add_instance_from_source('snort', 'tangosnort', interfaces=['input', 'output'])
         vim.add_link('client', 'cp0', 'firewall', 'input')
         vim.add_link('firewall', 'output', 'snort', 'input', sniff=True)
         vim.add_link('snort', 'output', 'server', 'cp0')
