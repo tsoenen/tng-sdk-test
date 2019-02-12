af75d1768fa71bc08e4f77631ca6238d74ac859e
Askhat Nuriddinov <askmyhat@yandex.ru>
2019-02-12 14:33:51 +0100

    No -t flag in pipeline docker

--- pipeline/clean.sh
+++ pipeline/clean.sh
@@ -2,7 +2,6 @@
 set -e
 
 cd ../
-rm -rf vim-emu
 rm -rf tng-sdk-test
 
 docker image rm vim-emu-test:latest tng-sdk-test:latest
--- pipeline/test.sh
+++ pipeline/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker run -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock tng-sdk-test pytest -vvv tests
+docker run -i --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock tng-sdk-test pytest -vvv tests
