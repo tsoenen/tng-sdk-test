6f81ebb4831725a51f1795cd741e13a348d3aafc
Askhat Nuriddinov <askmyhat@yandex.ru>
2019-02-12 14:09:21 +0100

    Build stage, no parent directory

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -2,10 +2,9 @@
 set -e
 
 # Build vim-emu docker image
-cd ..
 git clone https://osm.etsi.org/gerrit/osm/vim-emu.git
 cd ./vim-emu
 docker build -t vim-emu-test .
 
-cd ../tng-sdk-test
+cd ../
 docker build -t tng-sdk-test .
