ce21e3c70d5a47e0c3e22285adc79918a8147aaa
Askhat Nuriddinov <askmyhat@yandex.ru>
2019-04-25 16:48:04 +0200

    Tango package parser

--- tangotest/tangotools/package_parser.py
+++ tangotest/tangotools/package_parser.py
@@ -1,29 +1,54 @@
 import zipfile
 import yaml
+from collections import OrderedDict
 
 
-def parse_package(package_path)
+def parse_package(package_path, package_format='tango'):
+    if package_format == 'tango':
+        return parse_tango_package(package_path)
+    else:
+        raise Exception('Specified unsupported package_format {}. Supported formats: tango.'.format(package_format))
+
+def parse_tango_package(package_path)
+    # TODO: parametrize function type (virtual, cloud)
+
+    result = {}
+    nsd = None
+    nfds = {}
+
     with zipfile.ZipFile(package_path, "r") as z:
-        nsd_path = None
-        vnfd_paths = []
-
-        pd_path = 'TOSCA-Metadata/NAPD.yaml'
-        with z.open(pd_path) as pd_file:
+        with z.open('TOSCA-Metadata/NAPD.yaml') as pd_file:
             pd = yaml.safe_load(pd_file)
 
         for record in pd['package_content']:
             if record['content-type'] == 'application/vnd.5gtango.nsd':
-                if nsd_path:
+                if nsd:
                     raise Exception('Only one NSD can be in the package')
-                nsd_path = record['source']
+                with z.open(record['source']) as nsd_file:
+                    nsd = yaml.safe_load(nsd_file)
             if record['content-type'] == 'application/vnd.5gtango.vnfd':
-                vnfd_paths.append(record['source'])
+                with z.open(record['source']) as nfd_file:
+                    nfd = yaml.safe_load(nfd_file)
+                    nfds[nfd['name']] = nfd
 
-        with z.open(nsd_path) as nsd_file:
-            nsd = yaml.safe_load(nsd_file)
+    external_ns_cps = [cp['id'] for cp in nsd['connection_points'] if cp['type'] == 'external']
+    ns_virtual_links = [vl['connection_points_reference'] for vl in nsd['virtual_links']]
+    external_ns_virtual_links = [vl for vl in ns_virtual_links if set(vl) & set(external_ns_cps)]
+    external_nfs_cps = list(set(sum(external_ns_virtual_links, [])).difference(external_ns_cps))
 
-        for vnfd_path in vnfd_paths:
-            with z.open(vnfd_path) as vnfd_file:
-                vnfd = yaml.safe_load(vnfd_file)
-
+    for nf in nsd['network_functions']:
+        nf_name = nf['vnf_name']
+        nf_id = nf['vnf_id']
+        nfd = nfds[nf_name]
+        external_nf_cps = [cp.split(':')[1] for cp in external_nfs_cps if cp.split(':')[0] == nf_id]
+        nf_virtual_links = [vl['connection_points_reference'] for vl in nfd['virtual_links']]        
+        external_nf_virtual_links = [vl for vl in nf_virtual_links if set(vl) & set(external_nf_cps)]
+        external_dus_cps = list(set(sum(external_nf_virtual_links, [])).difference(external_nf_cps))
+        result[nf_name] = {}
+        for du in nfd['virtual_deployment_units']:
+            du_name = du['id']
+            external_du_cps = [cp.split(':')[1] for cp in external_dus_cps if cp.split(':')[0] == du_name]
+            result[nf_name][du_name] = external_du_cps
+            
+    return result
 
