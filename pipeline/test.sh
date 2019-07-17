d68e644ad8f41f732d8c42cd128ce9202f4d28e6
Askhat Nuriddinov <askmyhat@yandex.ru>
2019-07-17 16:42:09 +0200

    Test experiment

--- pipeline/test.sh
+++ pipeline/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker run -i --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock tng-sdk-test pytest -vvv tests
+docker run -i --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock tng-sdk-test pytest -vvv tests/test_instance.py::test_get_ip
